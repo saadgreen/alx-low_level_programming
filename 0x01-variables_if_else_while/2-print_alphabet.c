8600f99e3b7ad485731f316abddd4ca685aae086
saadgreen <alhyanesaad681@gmail.com>
2023-06-16 19:16:08 -0700

    saadon kive mrym

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
+* main - print all alphabets
  *
- * Return: Always 0 (success)
- */
-
+ * Return: 0
+*/
 int main(void)
 {
-char alphabet_lower = 'a';
-char alphabet_upper = 'A';
-while (alphabet_lower <= 'z')
+char alphabet = 'a';
+while (alphabet <= 'z')
 {
-	putchar(alphabet_lower);
-	alphabet_lower++;
-}
-while (alphabet_upper <= 'Z')
-{
-	write(1, &alphabet_upper, 1);
-	alphabet_upper++;
+	putchar(alphabet);
+	alphabet++;
 }
 putchar('\n');
 return (0);
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 /**
- * main - print all alphabets except e and q
+ * main - Entry point
  *
- * Return: 0
-*/
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+char alphabet_lower = 'a';
+char alphabet_upper = 'A';
+while (alphabet_lower <= 'z')
 {
-	if (alphabet != 'e' && alphabet != 'q')
-	putchar(alphabet);
+	putchar(alphabet_lower);
+	alphabet_lower++;
+}
+while (alphabet_upper <= 'Z')
+{
+	putchar(alphabet_upper);
+	alphabet_upper++;
 }
 putchar('\n');
 return (0);
-}
+}}
