b42eaec92be61485111e2de272a8992ccb811f0f
saadgreen <alhyanesaad681@gmail.com>
2023-08-03 08:00:30 -0700

    saad

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: listint_t
+ * Return: size_t
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num = 0;
+	long int diff;
+
+	while (head)
+	{
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (num);
+}
